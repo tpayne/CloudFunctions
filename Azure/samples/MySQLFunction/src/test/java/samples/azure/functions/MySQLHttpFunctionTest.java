a648215772ca1713a4e4569c29ac7516c65d3840
Tim Payne <tim.tpayne@gmail.com>
2021-04-17 20:31:50 +0100

    Update tests

--- Azure/samples/MySQLFunction/src/test/java/samples/azure/functions/MySQLHttpFunctionTest.java
+++ Azure/samples/MySQLFunction/src/test/java/samples/azure/functions/MySQLHttpFunctionTest.java
@@ -109,4 +109,53 @@ public class MySQLHttpFunctionTest {
         //assertTrue(ret.getBody().toString()
         //    .contains("<p><br>Clients<br><ol><li>0 NAME00 COMPANY00</li>"));    
     }
+
+    @Test
+    public void testHttpTriggerJsonTest() throws Exception {
+        // Setup
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        final Map<String, String> queryParams = new HashMap<>();
+        final Map<String, String> headers = new HashMap<>();
+
+        queryParams.put("host", "testdbmysql-server.mysql.database.azure.com");
+        queryParams.put("dbname", "testdbmysql");
+        queryParams.put("username", "mysqladmin@testdbmysql-server");
+        queryParams.put("passwd", "Ermd32_-1a2b3c"); // This is just random junk...
+        queryParams.put("jsontxt", "true"); // This is just random junk...
+        doReturn(queryParams).when(req).getQueryParameters();
+
+        headers.put("Content-Type", "application/json");
+        doReturn(headers).when(req).getHeaders();
+
+        final LinkedHashMap<String, String> queryBody = new LinkedHashMap<>();
+        queryBody.put("host", "testdbmysql-server.mysql.database.azure.com");
+        queryBody.put("dbname", "testdbmysql");
+        queryBody.put("username", "mysqladmin@testdbmysql-server");
+        queryBody.put("passwd", "Ermd32_-1a2b3c");
+        queryBody.put("jsontxt", "true");
+
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        // Invoke
+        final HttpResponseMessage ret = new MySQLHttpFunction().run(req, context);
+
+        // Verify
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+        assertTrue(ret.getBody().toString().contains("ERROR")); 
+        //assertTrue(ret.getBody().toString()
+        //    .contains("<p><br>Clients<br><ol><li>0 NAME00 COMPANY00</li>"));    
+    }
 }

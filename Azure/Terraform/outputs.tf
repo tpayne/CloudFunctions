ecdb833e34dbe905b037ee69b5adf693d19d91a8
tpayne <tim.tpayne@gmail.com>
2023-08-14 02:29:06 +0100

    Output to map

--- Azure/Terraform/outputs.tf
+++ Azure/Terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "app-url" {
-  value = [
-    for k, v in azurerm_linux_function_app.funcApp : lower("https://${v.name}.azurewebsites.net/")
-  ]
+  value = {
+    for v in azurerm_linux_function_app.funcApp : v.name => lower("https://${v.name}.azurewebsites.net/")
+  }
   description = "The URL(s) that the Azure function(s) get deployed to"
 }
